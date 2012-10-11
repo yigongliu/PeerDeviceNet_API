c6896d028c94af553f3277ccba129014e13881d2
yigongliu <peerdevicenet@gmail.com>
2012-10-10 17:34:51 -0700

    add developer api repo

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 PeerDeviceNet_API
 =================
 
-PeerDeviceNet developer APIs: intents, messenger and AIDL
+PeerDeviceNet, as a mobile framework, supports the following three functional areas for developing connected mobile apps:
+
+* Remote intenting. Intents are android's internal messaging bus to glue components such as activities and services. PeerDeviceNet's remote intenting extends this bus to connected devices, enables remote start of activities and services.
+* Group communication. Allow mobile apps set up simple multicast groups among peer devices to enable various communication patterns.
+* Device connection. Provide reusable component (Connection Manager) and API to connect peer devices.
+
+There are three native communication mechanisms used by android system components and apps: intents, messenger and IDL(aidl). PeerDeviceNet exposes the above mentioned functionalities thru all these three mechanisms. 
+
+Intents and messenger apis are message based. Intents use action name strings as message ids and messengers use integer message ids. They all package message data as items inside a bundle. Normally IDL(aidl) interfaces are RPC based. Following "messaging" paradigm,  all PeerDeviceNet IDL api methods are asynchronous, ie. marked as "oneway" in definition. And results or responses are returned thru callback/Handler interfaces.
+
+For more detailed descriptions, please refer to "Developer API introduction"(http://www.peerdevicenet.net/api.html).
+
---
+++ com/xconns/peerdevicenet/NetInfo.java
@@ -0,0 +1,103 @@
+package com.xconns.peerdevicenet;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class NetInfo implements Parcelable {
+	//network type names
+	public final static int NoNet = -1;
+	public final static int WiFi = 0;
+	public final static int WiFiDirect = 1;
+	public final static int Bluetooth = 2;
+	public final static int Mobile = 3;
+	public final static int Cloud = 4;
+	public final static int Other = 5;
+	public final static int NET_TYPES = 6;
+	
+	public final static String NetTypeName(int t) {
+		switch (t) {
+		case NoNet:
+			return "NoNet";
+		case WiFi:
+			return "Wi-Fi";
+		case WiFiDirect:
+			return "Wi-Fi Direct";
+		case Bluetooth:
+			return "Bluetooth";
+		case Mobile:
+			return "Mobile";
+		case Cloud:
+			return "Cloud";
+		case Other:
+			return "Other";
+		}
+		return null;
+	}
+	
+	//net info
+	public int type = 0;
+	public String name = null;
+	public String pass = null;
+	public byte[] info = null;
+	public String intfName = null;  //local interface name for this net
+	public String addr = null;  //addr of this host in this net
+	public boolean mcast = false;
+	
+	public NetInfo(int t, String n, String p, byte[] i, String in, String a, boolean m) {
+		type = t;
+		name = n;
+		pass = p;
+		info = i;
+		intfName = in;
+		addr = a;
+		mcast = m;
+	}
+
+	public static final Creator<NetInfo> CREATOR = new Creator<NetInfo>() {
+		public NetInfo[] newArray(int size) {
+			return new NetInfo[size];
+		}
+		
+		public NetInfo createFromParcel(Parcel in) {
+			return new NetInfo(in);
+		}
+	};
+	
+	public NetInfo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("type:%s, name:%s, addr:%s", type, name, addr);
+	}
+
+	public int describeContents() {
+		return 0;
+	}
+
+	public NetInfo(Parcel in) {
+		readFromParcel(in);
+	}
+	
+	public void readFromParcel(Parcel in) {
+		type = in.readInt();
+		name = in.readString();
+		pass = in.readString();
+		//in.readByteArray(info);
+		info = in.createByteArray();
+		intfName = in.readString();  //local interface name for this net
+		addr = in.readString();  //addr of this host in this net
+		mcast = (in.readByte()==0)?false:true;
+	}
+	
+	public void writeToParcel(Parcel out, int flags) {
+		out.writeInt(type);
+		out.writeString(name);
+		out.writeString(pass);
+		out.writeByteArray(info);
+		out.writeString(intfName);
+		out.writeString(addr);
+		out.writeByte((byte)(mcast?1:0));
+	}
+}
